d550a8aef08ad491b3c831a1ac93e55f45de7e6f
dimedical-corporativo <redessociales@dimedicalcorporativo.mx>
2025-11-29 16:41:53 -0600

    longest valid parentheses - 32

---
+++ java/LongestValidParentheses-32/Solution.java
@@ -0,0 +1,28 @@
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public int longestValidParentheses(String s) {
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        stack.push(-1);
+        int max = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.removeLast();
+                
+                if(stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    max = Math.max(max, i - stack.peek());   
+                }
+            }
+        }
+
+        return max;
+    }
+}
