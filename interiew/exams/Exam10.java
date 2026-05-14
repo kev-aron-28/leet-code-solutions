0b3c7250eacc07b18431209982060095b83ef45c
dimedical-corporativo <redessociales@dimedicalcorporativo.mx>
2026-05-14 15:53:18 -0600

    exam

--- interiew/exams/Exam10.java
+++ interiew/exams/Exam10.java
@@ -1,3 +1,10 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
 import trees.TreeNode;
 
 public class Exam10 {
@@ -23,5 +30,152 @@ public class Exam10 {
         return node.val + Math.max(leftBranch, rightBranch);
     }
 
+
+    private int wordLadder(String beginWord, String endWord, List<String> wordList) {
+        Set<String> set = new HashSet<>(wordList);
+
+        if(!set.contains(endWord)) return 0;
+
+        Queue<String> queue = new ArrayDeque<>();
+
+        queue.add(beginWord);
+
+        int level = 1;
+
+        while (!queue.isEmpty()) { 
+            int size = queue.size();
+
+            for(int i = 0; i < size; i++) {
+                String current = queue.poll();
+
+                if(current.equals(endWord)) return level;
+
+                char arr[] = current.toCharArray();
+
+                for(int j = 0; j < arr.length; j++) {
+                    char original = arr[j];
+
+                    for(char c = 'a'; c <= 'z'; c++) {
+                        arr[j] = c;
+                        String next = new String(arr);
+
+                        if(set.contains(next)) {
+                            queue.offer(next);
+                            set.remove(next);
+                        }
+                    }
+
+                    arr[j] = original;
+                }
+            }
+
+            level++;
+        }
+
+        return 0;
+    }
+
+
+    private int openTheLock(String deadEnds[], String target) {
+        Set<String> set = new HashSet<>(List.of(deadEnds));
+
+        Queue<String> queue = new ArrayDeque<>();
+        queue.add("0000");
+
+        Set<String> visited = new HashSet<>();
+        visited.add("0000");
+
+        int levels = 0;
+
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+
+            for(int i = 0; i < size; i++) {
+                String current = queue.poll();
+
+                if(current.equals(target)) return levels;
+
+                for(String next : getLockCombination(current)) {
+                    if(!set.contains(next) && !visited.contains(next)) {
+                        queue.offer(next);
+                        visited.add(next);
+                    }
+                }
+
+            }
+
+            levels++;
+        }
+
+        return -1;
+    }
+
+    private List<String> getLockCombination(String combination) {
+        List<String> result = new ArrayList<>();
+
+        for(int i = 0; i < 4; i++) {
+            char arr[] = combination.toCharArray();
+
+            arr[i] = (char)(((arr[i] - '0' + 1) % 10) + '0');
+            
+            result.add(new String(arr));
+
+            arr = combination.toCharArray();
+
+            arr[i] = (char)(((arr[i] - '0' + 9) % 10) + '0');
+            result.add(new String(arr));
+        }
+
+
+        return result;
+    } 
+
+
+    private boolean IsGraphBipartite(int graph[][]) {
+        int n = graph.length;
+        int color[] = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            if(color[i] == 0) {
+                if(!IsGraphBipartiteHelper(i, 1, color, graph)) return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean IsGraphBipartiteHelper(int node, int currentColor, int color[], int graph[][]) {
+        color[node] = currentColor;
+
+        for(int n : graph[node]) {
+            if(color[n] == 0) {
+                if(!IsGraphBipartiteHelper(n, -currentColor, color, graph)) return false;
+            } else if(color[n] == currentColor) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    private int townJudge(int n, int graph[][]) {
+        int t[] = new int[n + 1];
+
+        for(int trust[] : graph) {
+            int a = trust[0];
+            int b = trust[1];
+
+            t[a]--;
+            t[b]++;
+        }
+
+        for(int i = 1; i <= n; i++) {
+            if(t[i] == n - 1) return i;
+        }
+
+
+        return -1;
+    }
     
 }
